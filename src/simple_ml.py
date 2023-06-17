b5676d7c56018853d50c9f7d2e4de66478bdb0ad
Charles Peng <charlespeng1998@outlook.com>
2023-06-18 00:25:09 +0800

    implement parse_mnist

--- src/simple_ml.py
+++ src/simple_ml.py
@@ -45,9 +45,16 @@ def parse_mnist(image_filename, label_filename):
                 labels of the examples.  Values should be of type np.uint8 and
                 for MNIST will contain the values 0-9.
     """
-    ### BEGIN YOUR CODE
-    pass
-    ### END YOUR CODE
+    with gzip.open(f"data/{image_filename}", 'rb') as image_file:
+        image_file_content = image_file.read()
+        _, num_images, num_rows, num_cols = struct.unpack_from(">iiii", image_file_content)
+        X = np.frombuffer(image_file_content, dtype=np.uint8, offset=16).reshape(num_images, -1)
+
+    with gzip.open(f"data/{label_filename}", 'rb') as label_file:
+        label_file_content = label_file.read()
+        y = np.frombuffer(label_file_content, dtype=np.uint8, offset=8)
+    
+    return (X, y)
 
 
 def softmax_loss(Z, y):
