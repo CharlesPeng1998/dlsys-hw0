b8d6a07dc035696805cf7253ab7d21d484062a24
Charles Peng <charlespeng1998@outlook.com>
2023-07-01 12:28:27 +0800

    Implemented softmax regression cpp

--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -1,9 +1,91 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <cmath>
+#include <cstring>
 #include <iostream>
 
 namespace py = pybind11;
+
+// nk * km -> nm
+float* matrix_mutiply(const float *x0, const float *x1, size_t n, size_t k, size_t m) {
+    float* res = new float[n * m]();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            float sum = 0;
+            for (int kk = 0; kk < k; ++kk) {
+                sum += x0[i * k + kk] * x1[kk * m + j];
+            }
+            res[i * m + j] = sum;
+        }
+    }
+    return res;
+}
+
+void exp(float* x, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            float v = x[i * m + j];
+            x[i * m + j] = std::exp(v);
+        }
+    }
+}
+
+void normalize(float* x, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        float sum = 0;
+        for (int j = 0; j < m; ++j) {
+            sum += x[i * m + j];
+        }
+        for (int j = 0; j < m; ++j) {
+            x[i * m + j] /= sum;
+        }
+    }
+}
+
+float* one_hot(const unsigned char *y, size_t m, size_t k) {
+    float* res = new float[m * k]();
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < k; ++j) {
+            if (j == y[i]) res[i * k + j] = 1.0;
+            else res[i * k + j] = 0;
+        }
+    }
+    return res;
+}
+
+float* transpose(const float *x, size_t n, size_t m) {
+    float* res = new float[n * m]();
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            res[i * n + j] = x[j * m + i];
+        }
+    }
+    return res;
+}
+
+void subtract(float *x, float *y, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            x[i * m + j] -= y[i * m + j];
+        }
+    }
+}
+
+void div_scalar(float *x, float y, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            x[i * m + j] /= y;
+        }
+    }
+}
+
+void mul_scalar(float *x, float y, size_t n, size_t m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            x[i * m + j] *= y;
+        }
+    }
+}
 
 
 void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
@@ -31,10 +113,19 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
      * Returns:
      *     (None)
      */
-
-    /// BEGIN YOUR CODE
-
-    /// END YOUR CODE
+    for (int i = 0; i <= m - batch; i += batch) {
+        const float* x_batch = X[i * n];
+        const unsigned char* y_batch = y[i];
+        float* Z = matrix_mutiply(x_batch, theta, batch, n, k); // mn * nk = mk
+        exp(Z, batch, k);
+        normalize(Z, batch, k);
+        float* I_y = one_hot(y_batch, batch, k);
+        subtract(Z, I_y, batch, k);
+        float* gradient = matrix_mutiply(transpose(x_batch, batch, n), Z, n, batch, k);
+        div_scalar(gradient, batch, n, k);
+        mul_scalar(gradient, lr, n, k);
+        subtract(theta, gradient, n, k);
+    }
 }
 
 
